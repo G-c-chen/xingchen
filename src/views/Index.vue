daf7755cc075ef3cf94b987f3dada85f8c523194
GcChan <43054104+G-c-chen@users.noreply.github.com>
2022-07-25 13:34:20 +0800

    Update Index.vue

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -449,7 +449,7 @@ export default {
 }
 .common-pop {
   position: fixed;
-  width: 750px;
+  width: 100%;
   height: 100vh;
   top: 0;
   left: 0;
@@ -457,7 +457,7 @@ export default {
 
   .pop-mark {
     position: fixed;
-    width: 750px;
+    width: 100%;
     height: 100vh;
     background-color: rgba(0, 0, 0, 0.5);
     top: 0;
