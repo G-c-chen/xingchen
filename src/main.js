bb860ac9c67ddacd4a41217b6958b5ae63e1e38a
gcchen <gcchen@gcchendeMacBook-Air.local>
2022-08-05 22:26:20 +0800

    添加百度统计

--- src/main.js
+++ src/main.js
@@ -15,17 +15,6 @@ document.title = '星辰网络'
 
 Vue.config.productionTip = false;
 
-window.BJ_REPORT.init({
-    id: 27, // project_code
-    uin: "nuanliuguanwang", // 用户标识
-    url: "https://error.youjyi.cn/api/errrorlog/receive", // 上报url
-    random: 1, // 抽样上报
-    offlineLog: false, // 是否离线上报
-    onReport() {
-        // 上报回调
-    },
-});
-
 
 new Vue({
     router,
