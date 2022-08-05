9c493ae5acc225c56c6ae0dfc49ba57af52f4d91
gcchen <gcchen@gcchendeMacBook-Air.local>
2022-08-05 22:21:40 +0800

    添加百度统计

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import { isYqwHost, currentPro, currentType } from "./common/js/utils.js";
 import 'lib-flexible/flexible.js'
+import "vant/lib/index.css";
 import { List, Popup } from 'vant';
 import './common/less/mixin.less';
 Vue.use(List);
